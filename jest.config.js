2de5202e1a3edfb24f6f9f7b0243869853cde664
Shravani Sriramoju <shravanisriramoju@Shravanis-MacBook-Air.local>
2026-04-18 16:43:30 +0530

    fixed issues with babel config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/$1',
-      '\\.(jpg|jpeg|png|webp|svg)$': '<rootDir>/test/__mocks__/fileMock.js'
-    },
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest'
-    },
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
-  }
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(jpg|jpeg|png|webp|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+}
+
+module.exports = createJestConfig(customJestConfig)
